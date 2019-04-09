839ff4f7fe28351d5434c701ea7343113d9c35ea
Jimmy Wickström <jimmy@MBP51.local>
2019-04-10 00:09:03 +0200

    Add thread

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ from webdriverext import ChromeDriverExt
 from seleniumcommand import WebCommand
 import csv
 import time
+import xml.etree.ElementTree as ET
 
 class app(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self._event = threading.Event()
-        #f = open('template.csv', 'r')
         self.xc = WebCommand()
         self.list = []
-        self.list.append({'ops':'open', 'value':'https://google.com'})
-        self.list.append({'ops':'add','object':'search_box','value':'//input[@name="q"]'})
-        self.list.append({'ops':'wait','object':'search_box'})
-        self.list.append({'ops':'inj','object':'search_box','value':'Hello World'})
-        self.list.append({'ops':'submit','object':'search_box'})
-        self.list.append({'ops':'wait','object':'search_box'})
+        self.list.append({'page':'overview','event':'open','object':'null','value':'null'})
+        self.list.append({'page':'overview','event':'wait','object':'searchbox','value':'null'})
+        self.list.append({'page':'overview','event':'inject','object':'searchbox','value':'Hello World'})
+        self.list.append({'page':'overview','event':'submit','object':'searchbox','value':'null'})
+        self.list.append({'page':'overview','event':'wait','object':'searchbox','value':'null'})
+        self.list.append({'page':'services','event':'open','object':'null','value':'null'})
 
     def run(self):
         for cm in self.list:
@@ -31,16 +31,27 @@ class app(threading.Thread):
     def resume(self):
         self._event.set()
 
+class xmlTemplateParser():
+    def __init__(self):
+        self.xc = WebCommand()
+    def load(self, f):
+        e = ET.parse(f)
+        root = e.getroot()
+        for page in root.findall('page'):
+            print(page.get('name'))
+            self.xc.execute({'event':'addpage','page':page.get('name'),'url':page.get('url')})
+            for element in page.find('elements'):
+                print("- %s: %s: %s" % (page.get('name'), element.get('name'), element.text))
+                self.xc.execute({'page':page.get('name'),'event':'addelement','object':element.get('name'),'value':element.text})
+
 if __name__ == "__main__":
+    template = xmlTemplateParser()
+    template.load('template.xml')
     web = ChromeDriverExt().getInstance()
     print(hex(id(web)))
     ap = app()
     ap.resume()
     ap.start()
-    time.sleep(1)
-    ap.pause()
-    time.sleep(5)
-    ap.resume()
     ap.join()
     time.sleep(5)
     web.close()
