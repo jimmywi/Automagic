d0a0a5f50f870c6493b24238c253d9f0bc1e3cdf
Jimmy Wickström <jimmy@MBP51.local>
2019-04-10 00:15:07 +0200

    Add page.py to store page elements

---
+++ page.py
@@ -0,0 +1,13 @@
+
+class Page():
+    def __init__(self,c):
+        self.url = c['url']
+        self.elements = {}
+    def add_element(self,c):
+        self.elements[c['object']] = c['value']
+    def get_element(self,c):
+        return self.elements[c['object']]
+    def get_elements(self):
+        return self.elements
+    def get_url(self):
+        return self.url
