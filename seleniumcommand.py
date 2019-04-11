d250f79cdeb22fcb6ce1da43065d68d4ab5e8646
Jimmy Wickström <jimmy@MBP51.local>
2019-04-11 07:47:41 +0200

    move logic to webdriverext.py

--- seleniumcommand.py
+++ seleniumcommand.py
@@ -5,32 +5,31 @@ class WebCommand():
         driver = ChromeDriverExt().getInstance()
         def open(cm):
             print("Open: %s" % cm['page'])
-            driver.get(driver.getPage(cm))
+            driver.get(driver.get_page(cm))
         def addpage(cm):
             print("Page: %s" % (cm['page']) )
-            driver.addPage(cm)
+            driver.add_page(cm)
         def getpage(cm):
             print("Page: %s" % (cm['page']) )
+            driver.get_page(cm)
         def addelement(cm):
             print("Page: %s Object: %s Value: %s" % (cm['page'],cm['object'],cm['value']) )
-            driver.addElement(cm)
+            driver.add_element(cm)
         def getelement(cm):
             print("Page: %s" % cm['object'])
         def scroll(cm):
             print("Scroll: %s" % cm['object'])
         def submit(cm):
             print("Submit: %s" % cm['object'])
-            xpath = driver.getElement(cm)
-            el = driver.find_element_by_xpath(xpath)
-            el.submit()
+            driver.do_submit(cm)
         def inject(cm):
-            driver.injectText(cm)
+            driver.inject_text(cm)
         def click(cm):
             print("Click: %s" % cm['object'])
-            driver.waitUntilClickable(cm)
+            driver.wait_until_clickable(cm)
         def wait(cm):
             print("Wait: %s" % cm['object'])
-            driver.waitUntilElementLocated(cm)
+            driver.wait_until_element_located(cm)
         self.func_list = vars()
     def execute(self,cm):
         self.func_list[cm['event']](cm)
