4a0e8dc14594bf6540b80f509cd67b865672067e
Jimmy Wickström <jimmy@MBP51.local>
2019-04-17 22:24:47 +0200

    clean up code

--- webdriverext.py
+++ webdriverext.py
@@ -7,18 +7,20 @@ class WebDriverChromeExt(webdriver.Chrome):
     def __init__( self, path ):
         webdriver.Chrome.__init__( self, path )
         self.pages = {}
+
     def set_pages(self, pages):
         self.pages = pages
+
     def open_url(self,c):
-        print(c['page'])
         print(self.pages[c['page']].get_url())
         self.get(self.pages[c['page']].get_url())
+
     def do_submit(self,c):
-        print(c)
         xpath = self.pages[c['page']].get_element(c)
         self.wait_until_element_located(c)
         element = self.find_element(By.XPATH, xpath)
         element.submit()
+
     def inject_text(self, c):
         js_syntax = """
                     (document.evaluate('%s',document,null,
@@ -29,25 +31,36 @@ class WebDriverChromeExt(webdriver.Chrome):
         xpath = self.pages[c['page']].get_element(c)
         js_syntax = js_syntax % (xpath, c['value'])
         self.execute_script(js_syntax)
+
     def wait_until_clickable(self, c):
         print("Wait until clickable") 
         wait = WebDriverWait(self,10)
         xpath = self.pages[c['page']].get_element(c)
-        element = wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        element = wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH,xpath)
+            ))
+
     def wait_until_element_located(self, c):
         print("Wait until element located") 
         wait = WebDriverWait(self,10)
         xpath = self.pages[c['page']].get_element(c)
-        element = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+        element = wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH,xpath)
+            ))
+
 class Singleton(type):
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
 class ChromeDriverExt(metaclass=Singleton):
     def __init__(self):
         self.driver = None 
+
     def getInstance(self):
         if self.driver == None:
             chrome_path = './chromedriver'
